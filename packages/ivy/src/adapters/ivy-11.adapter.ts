ebea0ee165c33c1a1a44249ee40e12bcfdc3163f
Alex Dekau <alex.potatofarms@gmail.com>
2021-05-02 00:48:19 -0400

    first impl of getComponentTree

--- packages/ivy/src/adapters/ivy-11.adapter.ts
+++ packages/ivy/src/adapters/ivy-11.adapter.ts
@@ -1,9 +1,12 @@
 import { Ivy11Consts } from "../constants/ivy-11.const";
+import { NamedObject } from "../enums/named-objects.enum";
 import { findAngularRoot } from "../find-angular-root";
 import { Ivy11LView, Ivy11TView } from "../types";
+import { ComponentTree } from "../types/core/component-tree.type";
 import { IvyAdapter } from "../types/ivy-adapter";
 import { IvyConstants } from "../types/ivy-constants";
 import { isIvyRootContext } from "../util";
+import { namedArray } from "../util/named-object";
 
 export class Ivy11Adapter extends IvyAdapter {
     public version = 11;
@@ -42,6 +45,28 @@ export class Ivy11Adapter extends IvyAdapter {
     public getTView(from: Element | undefined): Ivy11TView | undefined {
         const ctx = this.getNgContext(from);
         return ctx?.[this.consts.lView.tView];
+    }
+
+    public getViewChildren(from: Ivy11LView | undefined): Ivy11LView[] | undefined {
+        const tView = from?.[this.consts.lView.tView];
+        if (!tView || !Array.isArray(tView.components)) {
+            return;
+        }
+        const components = tView.components.map((lViewComponentIndex) => from?.[lViewComponentIndex]);
+        return components;
+    }
+
+    public getComponentTree(from: Ivy11LView | undefined, build: any = {}): ComponentTree {
+        const comps = this.getViewChildren(from);
+        if (!comps) {
+            return build;
+        }
+        return namedArray(NamedObject.ComponentTree)(comps.reduce((agg, cur, i) => {
+            return {
+                ...agg,
+                [i]: { component: cur, children: this.getComponentTree(cur) }
+            };
+        }, {} as ComponentTree));
     }
 
     public getTemplate() {
